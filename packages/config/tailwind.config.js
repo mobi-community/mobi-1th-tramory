bb64b74a61ffbd634416d94842af6af5874bfde7
mobiorganization <mobiorganization@gmail.com>
2023-10-19 09:38:19 +0900

    init

---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    '../../packages/ui/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+  },
+  plugins: [require('tailwindcss-animate')],
+};
---
+++ packages/ui/components/index.ts
@@ -0,0 +1,3 @@
+export * from './theme-provider';
+export * from './ui-button';
+export * from './ui-dropdown-menu';
