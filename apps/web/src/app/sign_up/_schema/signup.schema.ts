601a06597375ebdfa85835aba7f85fccacf0d668
zivivle <zivivle@gmail.com>
2023-10-20 16:36:46 +0900

    :sparkles:feat: 리액트훅폼+yup 스키마 세팅

---
+++ apps/web/src/app/sign_up/_schema/signup.schema.ts
@@ -0,0 +1,43 @@
+import * as yup from 'yup';
+
+import { signupConstants } from '../../../constants/sign_up.constants';
+
+export const SIGNUP_SCHEMA = yup.object({
+  id: yup
+    .string()
+    .min(5, '5자 이상 입력해주세요.')
+    .max(12, '12자 이하로 입력해주세요.')
+    .matches(signupConstants.REGEX.id, {
+      message: '영문과 숫자만 입력해주세요.',
+      excludeEmptyString: true,
+    })
+    .required('아이디을 입력해주세요.'),
+
+  password: yup
+    .string()
+    .min(8, '8자 이상 입력해주세요.')
+    .max(16, '16자 이하로 입력해주세요.')
+    .matches(signupConstants.REGEX.password, {
+      message: '영문, 숫자, 특수문자를 조합해서 입력해주세요.',
+    })
+    .required('비밀번호를 입력해주세요'),
+
+  pwconfirm: yup
+    .string()
+    .min(8, '8자 이상 입력해주세요.')
+    .max(16, '16자 이하로 입력해주세요.')
+    .matches(signupConstants.REGEX.password, {
+      message: '영문, 숫자, 특수문자를 조합해서 입력해주세요.',
+    })
+    .required('비밀번호를 입력해주세요'),
+
+  Nickname: yup
+    .string()
+    .min(3, '3자 이상 입력해주세요.')
+    .max(15, '15자 이하로 입력해주세요.')
+    .matches(signupConstants.REGEX.nickName, {
+      message: '닉네임에 영문, 한글, 숫자만 입력해주세요.',
+      excludeEmptyString: true,
+    })
+    .required('아이디을 입력해주세요.'),
+});
--- apps/web/src/constants/sign_up.constants.ts
+++ apps/web/src/constants/sign_up.constants.ts
@@ -10,4 +10,9 @@ export const signupConstants = {
     subImage: subImage,
   },
   info: '회원가입 하기',
+  REGEX: {
+    id: /^[0-9a-zA-Z]+$/,
+    password: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-?~.,/])(?=.*[0-9]).{8,30}$/,
+    nickName: /^[가-힣0-9a-zA-Z]+$/,
+  },
 };
