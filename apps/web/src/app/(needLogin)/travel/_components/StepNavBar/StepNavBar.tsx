a061216f8df1e68e79a1e6a5b3ec83261ded0047
heesuePark <phs8743@naver.com>
2023-10-21 15:46:52 +0900

    :design: 기록&계획 등록  stepnavbar 컴포넌트  레이아웃 퍼블리싱 구현

---
+++ apps/web/src/app/(needLogin)/travel/_components/StepNavBar/StepNavBar.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import Link from 'next/link';
+
+import { stepArray } from '../../../../../constants/stepnavbar.constants';
+
+// * url 확정 후 기능 구현 시 추가할 사항
+// 기본 : border + primaryGray-300
+// 해당 url에 머무는 경우 : border + primaryBlue-400
+// 내용을 저장하고 다음 페이지로 넘어간 경우 : bg + primaryBlue-400
+
+const StepNavbar: React.FC = () => {
+  return (
+    <>
+      <div className='mt-10 flex h-20 items-center justify-center'>
+        <ul className='bg-primaryBlue-100 absolute  flex h-20 w-full max-w-[969px] justify-center '>
+          {stepArray.map(({ id, key, value, url }) => (
+            <Link href={url} key={id}>
+              <li
+                key={id}
+                className='mx-11 list-none'
+                style={{ position: 'relative', zIndex: 100 }}
+              >
+                <div className='border-primaryGray-300 text-primaryGray-300	mx-5 mt-5 flex h-10 w-10 items-center justify-center rounded-full border-4 bg-white text-xs font-semibold'>
+                  {key}
+                </div>
+                <div className='text-primaryGray-400 mx-4	mt-5 flex h-12 w-12 items-center justify-center font-semibold'>
+                  {value}
+                </div>
+              </li>
+            </Link>
+          ))}
+        </ul>
+        <div className='bg-primaryBlue-200 z-10 h-1 w-full max-w-[500px]'></div>
+      </div>
+    </>
+  );
+};
+
+export default StepNavbar;
---
+++ apps/web/src/constants/stepnavbar.constants.ts
@@ -0,0 +1,6 @@
+export const stepArray = [
+  { id: 0, key: '01', value: '제목', url: '/' },
+  { id: 1, key: '02', value: '언제', url: '/' },
+  { id: 2, key: '03', value: '무엇을', url: '/' },
+  { id: 3, key: '04', value: '어떻게', url: '/' },
+];
