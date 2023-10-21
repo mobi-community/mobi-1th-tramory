2f230c13ca39814c75c29ca2fc35c5fa43cf4a85
nasilKiM <nasikun@naver.com>
2023-10-21 17:42:18 +0900

    :truck:rename : 폴더설정 수정

--- apps/web/src/app/(needLogin)/travel/layout.tsx
+++ apps/web/src/app/(needLogin)/travel/layout.tsx
@@ -1,1 +1,16 @@
 // 레이아웃 처리 (공통 nav bar = StageIndicator등 )
+
+export default function TravelLayout({
+  children,
+}: {
+  children: React.ReactNode;
+  asideContent: React.ReactNode;
+}) {
+  return (
+    <>
+      <div>
+        <div>{children}</div>
+      </div>
+    </>
+  );
+}
