65b0b68b641382302a0817575de9a5fe81e54bff
JeongwooHam <hamjw0122@gmail.com>
2023-10-22 01:21:03 +0900

    💄 design: shadcn switch로 modeswitch button 구현

--- apps/web/src/components/Header/Header.tsx
+++ apps/web/src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
+import { Switch } from 'ui';
 
 import logo from '/public/assets/logo_black.svg';
 
@@ -9,16 +10,28 @@ import { headerConfig } from '../../constants';
 import materialIcon from '../../utils/materialIcon';
 
 const Header: React.FC = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const handleMode = () => {
+    setIsDarkMode((prev: boolean) => !prev);
+  };
+
   return (
-    <div className='z-20 ml-20 mt-10 flex justify-between'>
+    <div className='ml-20 mt-10 flex justify-between'>
       <div>
         <Image src={logo} alt='트래모리 로고' />
       </div>
-      <div>
+      <div className='flex'>
+        <div className='flex items-center space-x-2'>
+          <Switch
+            id='airplane-mode'
+            onClick={handleMode}
+            isDarkMode={isDarkMode}
+          />
+        </div>
         {headerConfig.logoArray.map((logo) =>
           materialIcon({
             iconName: logo,
-            style: 'text-primaryGray-400 cursor-pointer',
+            style: 'text-primaryGray-400 cursor-pointer pl-5',
             size: 32,
           })
         )}
