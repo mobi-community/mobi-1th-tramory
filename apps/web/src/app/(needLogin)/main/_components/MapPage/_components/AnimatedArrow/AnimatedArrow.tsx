6778a518013ce1443b7b4ae06185826a7d258b90
JeongwooHam <hamjw0122@gmail.com>
2023-10-22 22:59:11 +0900

    💄 design: framer-motion으로 애니메이션 구현

--- apps/web/src/app/(needLogin)/main/_components/MapPage/_components/AnimatedArrow/AnimatedArrow.tsx
+++ apps/web/src/app/(needLogin)/main/_components/MapPage/_components/AnimatedArrow/AnimatedArrow.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion';
+
 import { MapPageConfig } from '../../../../../../../constants';
 import materialIcon from '../../../../../../../utils/materialIcon';
 
@@ -5,9 +7,13 @@ export const AnimatedArrow: React.FC = () => {
   return (
     <div>
       {MapPageConfig.arrowText}
-      <div>
+      <motion.div
+        initial={{ scale: 1, y: 0 }}
+        animate={{ scale: 1, y: [0, 10, 0] }}
+        transition={{ repeat: Infinity, repeatDelay: 0.2 }}
+      >
         {materialIcon({ iconName: 'keyboard_double_arrow_down', size: 33 })}
-      </div>
+      </motion.div>
     </div>
   );
 };
