cdc9b83222b68c1d28fe3520f0284818534ce913
zivivle <zivivle@gmail.com>
2023-10-23 14:35:32 +0900

    :lipstick:design: shadcn 체크박스 추가 + 커스텀

--- apps/web/src/app/(needLogin)/mypage/my_story/plan/page.tsx
+++ apps/web/src/app/(needLogin)/mypage/my_story/plan/page.tsx
@@ -1,4 +1,4 @@
-import { RadioGroup, RadioGroupItem } from 'ui';
+import { Checkbox } from 'ui';
 
 import { MyPageContainer } from '../../_components';
 
@@ -6,16 +6,7 @@ const MyStoryPlanPage = () => {
   return (
     <div className='text-primaryBlue-700 ml-10 flex w-full flex-col items-center justify-center'>
       <MyPageContainer title='나의 스토리 - 여행 계획'>
-        <RadioGroup className='text-primaryBlue-700' defaultValue='option-one'>
-          <div className='flex items-center space-x-2'>
-            <RadioGroupItem value='option-one' id='option-one' />
-            <label htmlFor='option-one'>Option One</label>
-          </div>
-          <div className='flex items-center space-x-2'>
-            <RadioGroupItem value='option-two' id='option-two' />
-            <label htmlFor='option-two'>Option Two</label>
-          </div>
-        </RadioGroup>
+        <Checkbox id='terms2' />
       </MyPageContainer>
     </div>
   );
