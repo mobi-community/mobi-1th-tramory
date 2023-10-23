d93be85ab09a0db0a8bd7f255618df1790bfcd23
zivivle <zivivle@gmail.com>
2023-10-23 16:05:46 +0900

    :lipstick:design: MyStoryPlanCard 컴포넌트 퍼블리싱

---
+++ apps/web/src/app/(needLogin)/mypage/my_story/_mocks/planMockData.ts
@@ -0,0 +1,26 @@
+export const planDescription = [
+  {
+    title: '미국 LA 여행 계획',
+    location: 'United States, Los Angeles, CA',
+    date: '2023 9. 24 - 10. 4',
+    isRecord: true,
+  },
+  {
+    title: '태국 일주일 여행 계획',
+    location: 'Thailand, Bangkok',
+    date: '2022 6. 12 - 6. 20',
+    isRecord: false,
+  },
+  {
+    title: '미국 LA 여행 계획',
+    location: 'United States, Los Angeles, CA ',
+    date: '2023 9. 24 - 10. 4',
+    isRecord: false,
+  },
+  {
+    title: '미국 LA 여행 계획',
+    location: 'United States, Los Angeles, CA ',
+    date: '2023 9. 24 - 10. 4',
+    isRecord: false,
+  },
+];
--- apps/web/src/app/(needLogin)/mypage/my_story/plan/page.tsx
+++ apps/web/src/app/(needLogin)/mypage/my_story/plan/page.tsx
@@ -1,6 +1,8 @@
 import { Checkbox } from 'ui';
 
 import { MyPageContainer } from '../../_components';
+import { MyStoryPlanCard } from '../_components';
+import { planDescription } from '../_mocks';
 
 const MyStoryPlanPage = () => {
   return (
@@ -8,7 +10,12 @@ const MyStoryPlanPage = () => {
       <MyPageContainer title='나의 스토리 - 여행 계획'>
         <div className='mt-5 flex items-center px-12 text-[13px]'>
           <Checkbox id='planAll' />
-          <div className='ml-2'>지난 계획 모아보기</div>
+          <div className='ml-2 font-semibold'>지난 계획 모아보기</div>
+        </div>
+        <div className='flex flex-row flex-wrap justify-between px-12 pb-12 '>
+          {planDescription.map((planData) => (
+            <MyStoryPlanCard key={planData.title} planData={planData} />
+          ))}
         </div>
       </MyPageContainer>
     </div>
