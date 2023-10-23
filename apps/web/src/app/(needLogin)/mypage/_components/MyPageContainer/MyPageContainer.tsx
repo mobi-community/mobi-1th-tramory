b0e6820415402553c8f3f46000dd1d2d19d5c83c
zivivle <zivivle@gmail.com>
2023-10-23 14:51:08 +0900

    :lipstick:design: plan 페이지 타이틀, 컨테이너 퍼블리싱

--- apps/web/src/app/(needLogin)/mypage/_components/MyPageContainer/MyPageContainer.tsx
+++ apps/web/src/app/(needLogin)/mypage/_components/MyPageContainer/MyPageContainer.tsx
@@ -6,12 +6,18 @@ export const MyPageContainer: React.FC<MyPageContainerProps> = ({
   children,
   title,
 }) => {
+  const isMyStoryPanePage = title === '나의 스토리 - 여행 계획';
+
   return (
     <div className='h-auto w-[1024px] rounded-tl-[80px] shadow-[0_4px_20px_0_rgba(0,0,0,0.1)]'>
       {title && (
-        <div className='p-12'>
+        <div className={`${isMyStoryPanePage ? 'px-12 pt-12' : 'p-12'}`}>
           <p className='text-primaryBlue-700 mb-5 text-xl font-bold'>{title}</p>
-          <div className='bg-primaryGray-300 mb-12 h-px w-full'></div>
+          <div
+            className={`${
+              isMyStoryPanePage ? 'md-2' : 'mb-12'
+            } bg-primaryGray-300 h-px w-full`}
+          ></div>
         </div>
       )}
       {children}
--- apps/web/src/app/(needLogin)/mypage/my_story/plan/page.tsx
+++ apps/web/src/app/(needLogin)/mypage/my_story/plan/page.tsx
@@ -6,7 +6,10 @@ const MyStoryPlanPage = () => {
   return (
     <div className='text-primaryBlue-700 ml-10 flex w-full flex-col items-center justify-center'>
       <MyPageContainer title='나의 스토리 - 여행 계획'>
-        <Checkbox id='terms2' />
+        <div className='mt-5 flex items-center px-12 text-[13px]'>
+          <Checkbox id='planAll' />
+          <div className='ml-2'>지난 계획 모아보기</div>
+        </div>
       </MyPageContainer>
     </div>
   );
