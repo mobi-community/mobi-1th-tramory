4950436a51516f3c3ccfc612e771830fb16e6e13
yesoryeseul <yesoryeseul07@gmail.com>
2023-10-24 21:46:32 +0900

    💄 design: 내 방문 국가 모아보기 목데이터로 퍼블리싱

--- apps/web/src/app/(needLogin)/mypage/visit_country/my_visited/page.tsx
+++ apps/web/src/app/(needLogin)/mypage/visit_country/my_visited/page.tsx
@@ -1,12 +1,23 @@
-'use client';
+import { mypageNavConfig } from '@/constants';
+import { visitedContriesConfig } from '@/constants/visited_contries.contstants';
 
 import { MyPageContainer } from '../../_components';
+import OneMockFlag from './_components/OneMockFlag';
 
 const MyVisitedPage = () => {
+  const navTitle = mypageNavConfig.nav.find((nav) =>
+    nav.href.includes('visit_country')
+  ).title;
+
+  const mockData = visitedContriesConfig.tabs.map((tabs) => tabs)[0].continents;
+
   return (
     <div className='text-primaryBlue-700 ml-10 flex w-full flex-col items-center justify-center'>
-      <MyPageContainer title={'방문 국가'}>
-        <div></div>
+      <MyPageContainer title={navTitle}>
+        {/* 목데이터입니다. */}
+        {mockData.map((data, index) => (
+          <OneMockFlag data={data.continentData} key={index} />
+        ))}
       </MyPageContainer>
     </div>
   );
