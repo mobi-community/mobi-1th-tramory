4532630d1b1139d6e475df511c3a6ad2b4a006c2
nasilKiM <nasikun@naver.com>
2023-10-24 22:39:06 +0900

    :sparkles: Add : 플로팅 컴포넌트 및 모달창 url 연결처리

--- apps/web/src/app/(needLogin)/travel/plan/page.tsx
+++ apps/web/src/app/(needLogin)/travel/plan/page.tsx
@@ -1,10 +1,12 @@
 import { travelPlanStep1config } from '../../../../constants';
 import Step1Title from '../_components/Step1Title/Step1Title';
 
-export const travelPlanStep1 = () => {
+const travelPlanStep1 = () => {
   return (
     <div>
       <Step1Title config={travelPlanStep1config} />
     </div>
   );
 };
+
+export default travelPlanStep1;
--- apps/web/src/app/(needLogin)/travel/record/page.tsx
+++ apps/web/src/app/(needLogin)/travel/record/page.tsx
@@ -1,10 +1,12 @@
 import { travelRecordStep1config } from '../../../../constants';
 import Step1Title from '../_components/Step1Title/Step1Title';
 
-export default function travelRecordStep1() {
+const travelRecordStep1 = () => {
   return (
     <div>
       <Step1Title config={travelRecordStep1config} />
     </div>
   );
-}
+};
+
+export default travelRecordStep1;
