2d515282db9936132cbd6d5534c21ad3578bab34
heesuePark <phs8743@naver.com>
2023-10-24 12:43:01 +0900

    :lipstick: design : 카테고리 컴포넌트 퍼블리싱 완료

--- apps/web/src/app/(needLogin)/travel/_components/Step3What/components/Step3Category/Step3Category.tsx
+++ apps/web/src/app/(needLogin)/travel/_components/Step3What/components/Step3Category/Step3Category.tsx
@@ -1,5 +1,7 @@
 import { Controller } from 'react-hook-form';
 
+import { travelCategory } from '../../../../../../../constants/Step3Category.constants';
+
 const Step3Category = ({ title, control }) => {
   return (
     <>
@@ -8,13 +10,62 @@ const Step3Category = ({ title, control }) => {
         control={control}
         render={({ field }) => (
           <div>
-            <div
-              className='text-primaryGray-500 ml-[103px] mt-[25px] text-[30px] font-semibold'
-              onChange={(update) => {
-                field.onChange(update);
-              }}
-            >
+            <div className='text-primaryGray-500  mb-[10px] ml-[120px] mt-[25px] text-[30px] font-semibold'>
               {title}
+            </div>
+            <div className='flex'>
+              {travelCategory.slice(0, 3).map((cat) => (
+                <div
+                  key={cat.id}
+                  className='m-3 flex h-[50px] w-[170px] cursor-pointer rounded-sm bg-white'
+                  onClick={() => field.onChange(cat.label)}
+                >
+                  <div className='ml-[12px] flex items-center justify-center'>
+                    <span className='material-icons-outlined'>{cat.icon}</span>
+                  </div>
+                  <div>
+                    <div className='bg-primaryBlue-100 ml-[10px] mt-[-13px] h-[20px] w-[20px] rounded-[50%] '></div>
+                    <div className='text-primaryGray-300 ml-[5px] mt-[3px]  flex w-[32px] justify-center text-[22px]'>
+                      |
+                    </div>
+                    <div className='bg-primaryBlue-100 ml-[10px] mt-[2px] h-[20px] w-[20px] rounded-[50%]'></div>
+                  </div>
+                  <li
+                    className={`flex list-none items-center justify-center text-[19px] font-semibold ${
+                      cat.id == 1 ? 'ml-[30px]' : 'ml-[13px]'
+                    }`}
+                  >
+                    {cat.label}
+                  </li>
+                </div>
+              ))}
+            </div>
+            <div className='flex '>
+              {travelCategory.slice(3, 6).map((cat) => (
+                <div
+                  key={cat.id}
+                  className='m-3 flex h-[50px] w-[170px] cursor-pointer  rounded-sm bg-white'
+                  onClick={() => field.onChange(cat.label)}
+                >
+                  <div className='ml-[12px] flex items-center justify-center'>
+                    <span className='material-icons-outlined'>{cat.icon}</span>
+                  </div>
+                  <div>
+                    <div className='bg-primaryBlue-100 ml-[10px] mt-[-13px] h-[20px] w-[20px] rounded-[50%] '></div>
+                    <div className='text-primaryGray-300 ml-[5px] mt-[3px]  flex w-[32px] justify-center text-[22px]'>
+                      |
+                    </div>
+                    <div className='bg-primaryBlue-100 ml-[10px] mt-[2px] h-[20px] w-[20px] rounded-[50%] '></div>
+                  </div>
+                  <li
+                    className={`flex list-none items-center justify-center text-[19px] font-semibold ${
+                      cat.id == 3 ? 'ml-[10px]' : 'ml-[30px]'
+                    }`}
+                  >
+                    {cat.label}
+                  </li>
+                </div>
+              ))}
             </div>
           </div>
         )}
