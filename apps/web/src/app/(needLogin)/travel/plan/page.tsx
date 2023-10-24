34d09c5e7bf2bf191ecee9e367a90ae9dd7b42d8
heesuePark <phs8743@naver.com>
2023-10-24 18:15:51 +0900

    :recycle: refactor: plan 페이지 코드 삭제

--- apps/web/src/app/(needLogin)/travel/plan/page.tsx
+++ apps/web/src/app/(needLogin)/travel/plan/page.tsx
@@ -1,11 +1,10 @@
-import Step3What from '../_components/Step3What/Step3What';
 import TravelLayout from '../TravelLayout';
 
 export default function travelPlan() {
   return (
     <>
       <TravelLayout>
-        <Step3What title='어떤 여행을 즐기셨나요?' />
+        <div></div>
       </TravelLayout>
     </>
   );
