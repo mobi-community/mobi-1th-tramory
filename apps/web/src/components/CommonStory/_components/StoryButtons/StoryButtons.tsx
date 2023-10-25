c31c6370df02157e647de9a1cd37243b861d6262
JeongwooHam <hamjw0122@gmail.com>
2023-10-26 01:02:25 +0900

    ♻️ refactor: 버튼 위치 수정 및 코드 리팩토링

--- apps/web/src/components/CommonStory/_components/StoryButtons/StoryButtons.tsx
+++ apps/web/src/components/CommonStory/_components/StoryButtons/StoryButtons.tsx
@@ -1,14 +1,21 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import { Button } from 'ui';
 
 export const StoryButtons: React.FC = () => {
-  return (
-    <div className='mr-[35px] mt-[45px] flex'>
-      <Button variant='skyblue' className='h-[30px] w-[53px]'>
-        수정
-      </Button>
-      <Button variant='coralpink' className='ml-[5px] h-[30px] w-[53px]'>
-        삭제
-      </Button>
-    </div>
-  );
+  const pathName = usePathname();
+  const isMyStory = pathName.includes('mypage');
+
+  if (isMyStory)
+    return (
+      <div className='mr-[35px] mt-[45px] flex'>
+        <Button variant='skyblue' className='h-[30px] w-[53px]'>
+          수정
+        </Button>
+        <Button variant='coralpink' className='ml-[5px] h-[30px] w-[53px]'>
+          삭제
+        </Button>
+      </div>
+    );
 };
--- apps/web/src/components/CommonStory/_components/User/User.tsx
+++ apps/web/src/components/CommonStory/_components/User/User.tsx
@@ -1,7 +1,4 @@
-'use client';
-
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
 
 import type { storyUserType } from '../../CommonStory.types';
 import { StoryButtons } from '../StoryButtons';
@@ -12,26 +9,25 @@ export const User: React.FC<{ user: storyUserType }> = ({ user }) => {
     date?.getMonth() + 1
   }.${date?.getDate()}`;
 
-  const pathName = usePathname();
-  const isMyStory = pathName.includes('mypage');
-
   return (
-    <div className='flex justify-between'>
-      <div className='flex pt-[30px]'>
-        <div className='relative mr-[15px] h-[40px] w-[40px]'>
-          <Image
-            src={profileImage}
-            alt='사용자 프로필 이미지'
-            layout='fill'
-            className='rounded-[50%]'
-          />
+    <div>
+      <div className='flex justify-between'>
+        <div className='flex pt-[30px]'>
+          <div className='relative mr-[15px] h-[40px] w-[40px]'>
+            <Image
+              src={profileImage}
+              alt='사용자 프로필 이미지'
+              layout='fill'
+              className='rounded-[50%]'
+            />
+          </div>
+          <div className='mt-[3px]'>
+            <div className='text-sm'>{userId}</div>
+            <div className='text-primaryGray-300 text-xs'>{formattedDate}</div>
+          </div>
         </div>
-        <div className='mt-[3px]'>
-          <div className='text-sm'>{userId}</div>
-          <div className='text-primaryGray-300 text-xs'>{formattedDate}</div>
-        </div>
+        <StoryButtons />
       </div>
-      {isMyStory && <StoryButtons />}
       <div></div>
     </div>
   );
