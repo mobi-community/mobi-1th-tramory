47ea474332b0da6f74d251e98c3c4c55344f3429
zivivle <zivivle@gmail.com>
2023-10-25 14:45:04 +0900

    :lipstick:design: 정렬 및 필터 버튼 추가

--- apps/web/src/app/(needLogin)/mypage/_components/MyPageContainer/MyPageContainer.tsx
+++ apps/web/src/app/(needLogin)/mypage/_components/MyPageContainer/MyPageContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'ui';
 
 import type { MyPageContainerProps } from './MyPageContainer.types';
 
@@ -6,23 +7,27 @@ export const MyPageContainer: React.FC<MyPageContainerProps> = ({
   children,
   title,
 }) => {
-  const isMyStoryPlanePage = title === '나의 스토리 - 여행 계획';
   const isMyStoryRecordPage = title === '나의 스토리 - 여행 기록';
 
   return (
     <div className='h-auto w-[1024px] rounded-tl-[80px] shadow-[0_4px_20px_0_rgba(0,0,0,0.1)]'>
       {title && (
-        <div
-          className={`${
-            isMyStoryPlanePage || isMyStoryRecordPage ? 'px-12 pt-12' : 'p-12'
-          }`}
-        >
-          <p className='text-primaryBlue-700 mb-5 text-xl font-bold'>{title}</p>
-          <div
-            className={`${
-              isMyStoryPlanePage || isMyStoryRecordPage ? 'mb-2' : 'mb-12'
-            } bg-primaryGray-300 h-px w-full`}
-          ></div>
+        <div className='px-12 pt-12'>
+          <div className='flex'>
+            <p className='text-primaryBlue-700 mb-5 text-xl font-bold'>
+              {title}
+            </p>
+            {isMyStoryRecordPage && (
+              <Button
+                className='bg-primaryBlue-200 ml-4 mt-1 h-[22px] w-[90px] rounded-2xl border-none'
+                size='xsm'
+                variant='roundednavy'
+              >
+                정렬 및 필터
+              </Button>
+            )}
+          </div>
+          <div className='bg-primaryGray-300 h-px w-full'></div>
         </div>
       )}
       {children}
