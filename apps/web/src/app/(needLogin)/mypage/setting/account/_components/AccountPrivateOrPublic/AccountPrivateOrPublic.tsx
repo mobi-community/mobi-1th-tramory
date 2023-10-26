28f27ffc45a1f2a412947196a8962f17a5fc089c
yesoryeseul <yesoryeseul07@gmail.com>
2023-10-26 11:42:10 +0900

    💄 design: 계정 공개 컴포넌트 퍼블리싱

--- apps/web/src/app/(needLogin)/mypage/setting/account/_components/AccountPrivateOrPublic/AccountPrivateOrPublic.tsx
+++ apps/web/src/app/(needLogin)/mypage/setting/account/_components/AccountPrivateOrPublic/AccountPrivateOrPublic.tsx
@@ -1,4 +1,15 @@
+import { atom, useAtom } from 'jotai';
+import { Button } from 'ui';
+
+const isAccountPrivacyAtom = atom(false);
+
 export const AccountPrivateOrPublic = () => {
+  const [isAccountPrivacy, setIsAccountPrivacy] = useAtom(isAccountPrivacyAtom);
+
+  const handleChangeAccountPrivacy = () => {
+    setIsAccountPrivacy((prev) => !prev);
+  };
+
   return (
     <div>
       <div className='text-primaryGray-500 px-7 font-medium'>
@@ -13,25 +24,39 @@ export const AccountPrivateOrPublic = () => {
             * 스토리를 공개 설정하여도, 계정 비공개 상태에서는 공개되지
             않습니다.
           </p>
-          <div className='flex'>
-            <div className='mr-5'>
-              <input
-                id='accountPublic'
-                name='radio'
-                type='radio'
-                className='accent-primaryBlue-700 mr-2'
-              />
-              <label htmlFor='accountPublic'>예</label>
+          <div className='ml-2 mt-4 flex items-center justify-between'>
+            <div className='flex'>
+              <div className='mr-5'>
+                <input
+                  id='accountPublic'
+                  name='radio'
+                  type='radio'
+                  className='accent-primaryBlue-default mr-2'
+                  checked={isAccountPrivacy}
+                  onChange={handleChangeAccountPrivacy}
+                />
+                <label htmlFor='accountPublic'>예</label>
+              </div>
+              <div>
+                <input
+                  id='accountPrivate'
+                  name='radio'
+                  type='radio'
+                  className='accent-primaryBlue-default mr-2'
+                  checked={!isAccountPrivacy}
+                  onChange={handleChangeAccountPrivacy}
+                />
+                <label htmlFor='accountPrivate'>아니오</label>
+              </div>
             </div>
-            <div>
-              <input
-                id='accountPrivate'
-                name='radio'
-                type='radio'
-                className='accent-primaryBlue-700 mr-2'
-              />
-              <label htmlFor='accountPrivate'>아니오</label>
-            </div>
+            <Button
+              variant='defaultnavy'
+              weight='bold'
+              shape='full'
+              className='w-[200px] text-white'
+            >
+              계정공개 여부 변경하기
+            </Button>
           </div>
         </div>
       </div>
