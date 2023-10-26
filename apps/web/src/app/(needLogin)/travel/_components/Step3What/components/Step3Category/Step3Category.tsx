3c2b210285f1b626e98da604fd85d25183505494
heesuePark <phs8743@naver.com>
2023-10-26 16:34:00 +0900

    :recycle: refactor : 카테고리 코드 리팩터링

--- apps/web/src/app/(needLogin)/travel/_components/Step3What/components/Step3Category/Step3Category.tsx
+++ apps/web/src/app/(needLogin)/travel/_components/Step3What/components/Step3Category/Step3Category.tsx
@@ -21,11 +21,11 @@ const Step3Category = ({ title, control }) => {
             <div className='text-primaryGray-500  mb-[10px] ml-[120px] mt-[60px] text-[30px] font-semibold'>
               {title}
             </div>
-            <div className='mt-[20px] flex'>
-              {travelCategory.slice(0, 3).map((category) => (
+            <div className='mt-[20px] flex w-[600px] flex-wrap justify-center '>
+              {travelCategory.map((category) => (
                 <div
                   key={category.id}
-                  className={`m-3 flex h-[50px] w-[170px] cursor-pointer rounded-sm  ${
+                  className={`m-3 flex h-[50px] w-[175px] cursor-pointer rounded-sm  ${
                     selectedCategoryId === category.id
                       ? 'bg-[#AFCDF2]'
                       : 'bg-white'
@@ -49,43 +49,11 @@ const Step3Category = ({ title, control }) => {
                   </div>
                   <li
                     className={`flex list-none items-center justify-center text-[19px] font-semibold ${
-                      category.id == 1 ? 'ml-[30px]' : 'ml-[13px]'
-                    }`}
-                  >
-                    {category.label}
-                  </li>
-                </div>
-              ))}
-            </div>
-            <div className='flex '>
-              {travelCategory.slice(3, 6).map((category) => (
-                <div
-                  key={category.id}
-                  className={`m-3 flex h-[50px] w-[170px] cursor-pointer rounded-sm  ${
-                    selectedCategoryId === category.id
-                      ? 'bg-[#AFCDF2]'
-                      : 'bg-white'
-                  }`}
-                  onClick={() => {
-                    field.onChange(category.label);
-                    setSelectedCategoryId(category.id);
-                  }}
-                >
-                  <div className='ml-[12px] flex items-center justify-center'>
-                    <span className='material-icons-outlined'>
-                      {category.icon}
-                    </span>
-                  </div>
-                  <div>
-                    <div className='bg-primaryBlue-100 ml-[10px] mt-[-13px] h-[20px] w-[20px] rounded-[50%] '></div>
-                    <div className='text-primaryGray-300 ml-[5px] mt-[3px]  flex w-[32px] justify-center text-[22px]'>
-                      |
-                    </div>
-                    <div className='bg-primaryBlue-100 ml-[10px] mt-[2px] h-[20px] w-[20px] rounded-[50%] '></div>
-                  </div>
-                  <li
-                    className={`flex list-none items-center justify-center text-[19px] font-semibold ${
-                      category.id == 3 ? 'ml-[10px]' : 'ml-[30px]'
+                      category.id == 2 || category.id == 3
+                        ? 'ml-[7px] '
+                        : category.id == 0
+                        ? 'ml-[15px] '
+                        : 'ml-[23px] '
                     }`}
                   >
                     {category.label}
---
+++ apps/web/src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import Step3What from '../(needLogin)/travel/_components/Step3What/Step3What';
+
+const Test = () => {
+  return (
+    <div>
+      <Step3What title='무엇을' />
+    </div>
+  );
+};
+
+export default Test;
