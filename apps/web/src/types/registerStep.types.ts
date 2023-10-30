5cac0e5ff7afd5b152a2d4ec666d2ce4ae207e9e
heesuePark <phs8743@naver.com>
2023-10-30 15:57:30 +0900

    :bug: fix : 불필요한 코드 삭제

--- apps/web/src/types/registerStep.types.ts
+++ apps/web/src/types/registerStep.types.ts
@@ -1,11 +1,3 @@
-import { Control } from 'react-hook-form';
-
 export interface IregisterFormvalue {
   [key: string]: string;
-}
-
-export interface IRegisterProps {
-  control: Control<IregisterFormvalue>;
-  id?: number;
-  name?: string;
 }
