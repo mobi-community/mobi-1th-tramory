744e16b47d2e0564d5efad17c09c8a246a49a9c0
zivivle <zivivle@gmail.com>
2023-10-31 17:37:23 +0900

    :sparkles:feat: nextauth 미들웨어 적용

---
+++ apps/web/src/constants/protected_URL.ts
@@ -0,0 +1,10 @@
+export const PROTECTED_URL = [
+  '/mypage',
+  '/map',
+  '/travel',
+  '/recommend_country',
+  '/otherspage',
+  '/story_community',
+];
+
+export const PROTECTED_URL_LOGIN = ['/login', '/landing', '/sign_up'];
