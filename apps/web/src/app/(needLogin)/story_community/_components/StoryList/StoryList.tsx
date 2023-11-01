41128312de70e45ae4969467cb4baea8a671338a
JeongwooHam <hamjw0122@gmail.com>
2023-11-02 02:13:01 +0900

    ✨ feat: 지도 페이지 서치바 검색 결과 스토리 커뮤니티 페이지에 적용

--- apps/web/src/app/(needLogin)/story_community/_components/StoryList/StoryList.tsx
+++ apps/web/src/app/(needLogin)/story_community/_components/StoryList/StoryList.tsx
@@ -1,9 +1,16 @@
 'use client';
 
 import { useAtom, useAtomValue } from 'jotai';
+import Link from 'next/link';
+import { Button } from 'ui';
 
 import { CommonStory, Pagination } from '@/components';
-import { selectedCategoryAtom, storyCommunityAtoms } from '@/store';
+import { storyCommunityPageConfig } from '@/constants';
+import {
+  searchKeywordAtom,
+  selectedCategoryAtom,
+  storyCommunityAtoms,
+} from '@/store';
 
 import { storyMock } from '../../_mocks';
 
@@ -12,21 +19,33 @@ export const StoryList: React.FC = () => {
     storyCommunityAtoms.storyPageAtom
   );
 
-  const selectedCategory = useAtomValue(selectedCategoryAtom);
+  const [selectedCategory, setSelectedCategory] = useAtom(selectedCategoryAtom);
 
-  const storyArray =
+  const searchKeyword = useAtomValue(searchKeywordAtom);
+
+  const searchedArray = searchKeyword
+    ? storyMock.filter(
+        (story) =>
+          story.content.title.includes(searchKeyword) ||
+          story.content.text.includes(searchKeyword) ||
+          story.content.tags.some((tag) => tag.includes(searchKeyword)) ||
+          story.user.userId.includes(searchKeyword)
+      )
+    : storyMock;
+
+  const filteredStoryArray =
     selectedCategory && selectedCategory !== '전체'
-      ? storyMock.filter((story) => story.content.category === selectedCategory)
-      : storyMock;
+      ? searchedArray.filter(
+          (story) => story.content.category === selectedCategory
+        )
+      : searchedArray;
 
-  return (
-    <div className='relatve'>
-      <div className='flex justify-center'>
-        <div className='m-auto grid grid-cols-2 gap-8 gap-x-[5%]'>
-          {storyArray.map((story) => (
-            <CommonStory story={story} key={Math.random() * 1000} />
-          ))}
-        </div>
+  const HaveData = (
+    <div>
+      <div className='m-auto grid grid-cols-2 gap-8 gap-x-[5%]'>
+        {filteredStoryArray.map((story) => (
+          <CommonStory story={story} key={Math.random() * 1000} />
+        ))}
       </div>
       <div className='mt-[80px] flex h-[100px] justify-center'>
         <Pagination
@@ -36,6 +55,27 @@ export const StoryList: React.FC = () => {
           testData={100}
           bgColor='gray'
         />
+      </div>
+    </div>
+  );
+
+  const handleClearCategory = () => setSelectedCategory('전체');
+
+  const NotHaveData = (
+    <div className='text-center'>
+      <div className='my-20 text-xl font-bold'>
+        {storyCommunityPageConfig.noDataText}
+      </div>
+      <Link href={'./story_community'} onClick={handleClearCategory}>
+        <Button>{storyCommunityPageConfig.noDataButtonText}</Button>
+      </Link>
+    </div>
+  );
+
+  return (
+    <div className='relatve'>
+      <div className='flex justify-center'>
+        {filteredStoryArray.length ? HaveData : NotHaveData}
       </div>
     </div>
   );
