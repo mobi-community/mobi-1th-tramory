c587fc822d51725f9aebdd2d0a496437ae7e5c14
nasilKiM <nasikun@naver.com>
2023-11-02 19:08:40 +0900

    :sparkles: Add : 파일구조변경

--- apps/web/src/app/(needLogin)/travel/plan/page.tsx
+++ apps/web/src/app/(needLogin)/travel/plan/page.tsx
@@ -1,24 +1,24 @@
-'use client';
-import { useSearchParams } from 'next/navigation';
+// 'use client';
+// import { useSearchParams } from 'next/navigation';
 
-import { travelPlanStep1config } from '@/constants';
+// import { travelPlanStep1config } from '@/constants';
 
-import Step1Title from '../_components/Step1Title/Step1Title';
-import TravelPlanTemplete from './stepTemplete';
+// import Step1Title from '../_components/Step1Title/Step1Title';
+// import TravelPlanTemplete from './stepTemplete';
 
-const TravelPlan = () => {
-  const params = useSearchParams();
-  const search = params.get('stepId');
+// const TravelPlan = () => {
+//   const params = useSearchParams();
+//   const search = params.get('stepId');
 
-  let StepComponent;
+//   let StepComponent;
 
-  if (search == '0') {
-    StepComponent = <Step1Title config={travelPlanStep1config} />;
-  } else {
-    StepComponent = <TravelPlanTemplete search={search} />;
-  }
+//   if (search == '0') {
+//     StepComponent = <Step1Title config={travelPlanStep1config} />;
+//   } else {
+//     StepComponent = <TravelPlanTemplete search={search} />;
+//   }
 
-  return <div>{StepComponent}</div>;
-};
+//   return <div>{StepComponent}</div>;
+// };
 
-export default TravelPlan;
+// export default TravelPlan;
---
+++ apps/web/src/app/(needLogin)/travel/plan2/travelPlanTemplete.tsx
@@ -0,0 +1,25 @@
+import { travelPlanStep5config } from '@/constants';
+import { travelPlanStep2config } from '@/constants/travelStep2.constants';
+import { travelPlanStep3config } from '@/constants/travelStep3.constants';
+import { travelPlanStep4config } from '@/constants/travelStep4.constants';
+
+import Step2When from '../_components/Step2When/Step2When';
+import Step3What from '../_components/Step3What/Step3What';
+import Step4How from '../_components/Step4How/Step4How';
+import Step5Sumup from '../_components/Step5Sumup/Step5Sumup';
+import TravelLayout from '../TravelLayout';
+
+const COMPONENTS_MAP = {
+  '1': <Step2When config={travelPlanStep2config} />,
+  '2': <Step3What config={travelPlanStep3config} />,
+  '3': <Step4How config={travelPlanStep4config} />,
+  '4': <Step5Sumup config={travelPlanStep5config} />,
+};
+
+const TravelPlanTemplete = ({ search }) => {
+  const StepComponent = COMPONENTS_MAP[search];
+
+  return <TravelLayout>{StepComponent}</TravelLayout>;
+};
+
+export default TravelPlanTemplete;
