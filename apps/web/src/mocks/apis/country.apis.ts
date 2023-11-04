b04315cb16be5e38e1a54c732dd3252823852343
JeongwooHam <hamjw0122@gmail.com>
2023-11-05 01:09:27 +0900

    ✨ feat: 국가 정보 모달 msw 로직 생성

--- apps/web/src/mocks/apis/country.apis.ts
+++ apps/web/src/mocks/apis/country.apis.ts
@@ -1,5 +1,10 @@
 import { rest } from 'msw';
 
+import {
+  cityDataMock,
+  countryDataMock,
+} from '@/app/(needLogin)/recommend_country/_mocks';
+
 export const getCountryData = rest.get('/api/countries', (req, res, ctx) => {
   return res(
     ctx.status(200),
@@ -8,3 +13,47 @@ export const getCountryData = rest.get('/api/countries', (req, res, ctx) => {
     })
   );
 });
+
+export const getCountryInfo = rest.get(
+  '/api/country/info/:country',
+  (req, res, ctx) => {
+    const country = req.params.country;
+
+    const data = {
+      ...countryDataMock,
+      countryEng: country,
+      flagImage: '/public/images/이탈리아.gif',
+    };
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        success: true,
+        message: 'success',
+        data,
+      })
+    );
+  }
+);
+
+export const getCityInfo = rest.get(
+  '/api/country/info/:city',
+  (req, res, ctx) => {
+    const city = req.params.city;
+
+    const data = {
+      ...cityDataMock,
+      cityEng: city,
+      flagImage: '/public/images/이탈리아.gif',
+    };
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        success: true,
+        message: 'success',
+        data,
+      })
+    );
+  }
+);
