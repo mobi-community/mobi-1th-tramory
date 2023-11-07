6505ce0edac4dfbc75c851efa2782969a707fac9
nasilKiM <nasikun@naver.com>
2023-11-08 01:08:47 +0900

    :bug: Error Fix : simple modal 안열리는 오류 수정

--- apps/web/src/components/ModalDefault/TravelModalDefault.tsx
+++ apps/web/src/components/ModalDefault/TravelModalDefault.tsx
@@ -14,9 +14,9 @@ const TravelModalDefault = ({
 
   return (
     <div className='fixed left-0 top-0 z-[200] flex h-full w-full items-center justify-center bg-black bg-opacity-60'>
-      <div className='m-auto flex flex-col rounded-lg border border-gray-300 bg-white p-5 shadow-md'>
+      <div className='m-auto flex flex-col rounded-lg border border-gray-300 bg-white px-5 pb-8 shadow-md'>
         <div className='mb-1 flex justify-end'>
-          <button className='h-6 w-6 cursor-pointer' onClick={onClose}>
+          <button className='mt-3 h-6 w-6 cursor-pointer' onClick={onClose}>
             <span className='material-icons-outlined'>close</span>
           </button>
         </div>
--- apps/web/src/components/SimpleRecordModal/SimpleRecordModal.tsx
+++ apps/web/src/components/SimpleRecordModal/SimpleRecordModal.tsx
@@ -5,45 +5,46 @@ import { useForm } from 'react-hook-form';
 import { Button } from 'ui';
 
 import { openSimpleRecordModalAtom } from '../../store/simpleRecordModal.atom';
-import TravelModalDefault from '../ModalDefault/TravelModalDefault';
 import ValidatorCalendar from '../ValidatorCalendar/ValiedatorCaleander';
 import ValidateSelect from './_components/ValidateSelect/ValidateSelect';
 
-const SimpleRecordModal = () => {
+const SimpleRecordModal = ({ closeModal }) => {
   const { handleSubmit, control } = useForm();
   const onSubmit = (data) => console.log(data);
-  const [openSimpleRecordModal, setOpenSimpleRecordModal] = useAtom(
-    openSimpleRecordModalAtom
-  );
+  const [, setOpenSimpleRecordModal] = useAtom(openSimpleRecordModalAtom);
 
-  const closeModal = () => setOpenSimpleRecordModal(false);
+  const handleCloseModal = () => {
+    setOpenSimpleRecordModal(false);
+    if (closeModal) closeModal();
+  };
 
   return (
     <>
-      <TravelModalDefault isOpen={openSimpleRecordModal} onClose={closeModal}>
-        <div className='ml-5 text-2xl font-semibold	text-gray-600'>
-          간편 기록하기
+      <div className='ml-5 text-2xl font-semibold	text-gray-600'>
+        간편 기록하기
+      </div>
+      <p className='text-primaryGreen ml-80 text-sm'>*은 필수 항목입니다.</p>
+      <hr className='ml-5 mt-2 w-full max-w-[420px] border border-gray-300' />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='relative z-10'>
+          <ValidateSelect control={control} />
         </div>
-        <p className='text-primaryGreen ml-80 text-sm'>*은 필수 항목입니다.</p>
-        <hr className='ml-5 mt-2 w-full max-w-[420px] border border-gray-300' />
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className='relative z-10'>
-            <ValidateSelect control={control} />
+        <div className='text-primaryGray-500 ml-5  mt-7 flex'>
+          일자<p className='text-primaryGreen mb-1 ml-1'>*</p>
+        </div>
+        <ValidatorCalendar control={control} name='date' />
+        <div className='mt-14 flex justify-center'>
+          <div
+            onClick={handleCloseModal}
+            className='mr-4 mt-[10px] cursor-pointer text-[13px] font-semibold'
+          >
+            취소
           </div>
-          <div className='text-primaryGray-500 ml-5  mt-7 flex'>
-            일자<p className='text-primaryGreen mb-1 ml-1'>*</p>
-          </div>
-          <ValidatorCalendar control={control} name='date' />
-          <div className='mt-14 flex justify-center'>
-            <div className='mr-4 mt-[10px] cursor-pointer text-[13px] font-semibold'>
-              취소
-            </div>
-            <Button className='mb-4 ml-4 h-[35px] w-[120px] rounded-sm text-[13px] font-semibold'>
-              등록하기
-            </Button>
-          </div>
-        </form>
-      </TravelModalDefault>
+          <Button className='mb-4 ml-4 h-[35px] w-[120px] rounded-sm text-[13px] font-semibold'>
+            등록하기
+          </Button>
+        </div>
+      </form>
     </>
   );
 };
