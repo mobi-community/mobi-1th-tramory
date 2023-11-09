4451c11505ba4fc441e7259561178a08dc403d54
nasilKiM <nasikun@naver.com>
2023-11-10 00:55:20 +0900

    :lipstick: Design : 디자인 수정중

--- apps/web/src/app/(needLogin)/travel/_components/Step2When/Step2When.tsx
+++ apps/web/src/app/(needLogin)/travel/_components/Step2When/Step2When.tsx
@@ -5,7 +5,7 @@ import { useForm } from 'react-hook-form';
 import { formModePlanAtom, formModeRecordAtom } from '@/store';
 import { registerStateAtom } from '@/store/travelState.atom';
 
-import type { IStep2Props } from '../../Travel.type';
+import { IStep2Props } from '../../Travel.typs';
 import NavigateButton from '../NavigateButton/NavigateButton';
 import Step2Calendar from './components/Step2Calendar/Step2Calendar';
 
@@ -34,7 +34,7 @@ const Step2When: React.FC<IStep2Props> = ({ config }) => {
       <div className='mt-[57px] flex h-[600px] items-center justify-center '>
         <div className='bg-primaryBlue-100 absolute flex h-[600px] w-full max-w-[969px] justify-center '>
           <div>
-            <div className='text-primaryGray-500 ml-[120px] mt-[25px] text-[30px] font-semibold'>
+            <div className='text-primaryGray-500 my-3 h-[50px] w-full text-center text-[30px] font-semibold'>
               {config.label}
             </div>
             <div className='mt-[14px]'>
