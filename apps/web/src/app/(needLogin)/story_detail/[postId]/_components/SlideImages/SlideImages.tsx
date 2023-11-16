2b237baab76329d872053d80652fbc6d212d3a2f
JeongwooHam <hamjw0122@gmail.com>
2023-11-16 16:10:30 +0900

    💄 design: 슬라이드 리사이징

--- apps/web/src/app/(needLogin)/story_detail/[postId]/_components/SlideImages/SlideImages.tsx
+++ apps/web/src/app/(needLogin)/story_detail/[postId]/_components/SlideImages/SlideImages.tsx
@@ -32,11 +32,11 @@ export const SlideImages: React.FC<SlideImagesProps> = ({ targetStory }) => {
           iconName: 'arrow_back_ios',
           size: 50,
           style:
-            'prev-button cursor-pointer text-primaryGray-300 hover:text-white absolute left-0 top-[140px] z-10',
+            'prev-button cursor-pointer text-primaryGray-300 hover:text-white absolute left-0 top-[70px] z-10',
         })}
         {images.map((image, i) => (
           <SwiperSlide key={Math.random() * 10000}>
-            <div className={`h-[350px] w-[200px]`} key={Math.random() * 1000}>
+            <div className={`h-[200px] w-[350px]`} key={Math.random() * 1000}>
               <Image src={image} alt={'여행 이미지' + i} fill priority={true} />
             </div>
           </SwiperSlide>
@@ -45,7 +45,7 @@ export const SlideImages: React.FC<SlideImagesProps> = ({ targetStory }) => {
           iconName: 'arrow_forward_ios',
           size: 50,
           style:
-            'next-button cursor-pointer text-primaryGray-300 hover:text-white absolute right-0 top-[140px] z-10',
+            'next-button cursor-pointer text-primaryGray-300 hover:text-white absolute right-0 top-[70px] z-10',
         })}
       </Swiper>
     </div>
@@ -53,7 +53,7 @@ export const SlideImages: React.FC<SlideImagesProps> = ({ targetStory }) => {
 
   const oneImage = (
     <div
-      className={`relative my-10 h-[350px] w-[350px]`}
+      className={`relative my-10 h-[200px] w-[350px]`}
       key={Math.random() * 1000}
     >
       <Image src={images[0]} alt='여행 이미지' fill priority={true} />
